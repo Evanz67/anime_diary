231a994aec8cacca2a0b71b1e35fce73bd3e1b36
Hanz Christian Magtanong <hanzmagtanong67@yahoo.com>
2026-02-20 03:08:17 -0700

    added anime list

--- app/anime_list/page.js
+++ app/anime_list/page.js
@@ -1,7 +1,30 @@
+"use client"
+
+import { AnimeListTable } from "@/components/custom/anime_list_table"
+
 export default function AnimeList() {
+
+  const data = [
+    { id: 1, anime: "Naruto", entries: 50 },
+    { id: 2, anime: "One Piece", entries: 70 },
+    { id: 3, anime: "Attack on Titan", entries: 30 },
+    { id: 4, anime: "My Hero Academia", entries: 40 },
+    { id: 5, anime: "Demon Slayer", entries: 25 },
+  ]
+
+  const columns = [
+    { key: "anime", name: "Anime Series" },
+    { key: "entries", name: "# of Entries" },
+  ]
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Anime List</h1>
+    <div className="container mx-auto flex-1 p-4">
+      <h1 className="text-2xl font-bold mb-6">Anime Finished</h1>
+        <AnimeListTable 
+          columns={columns} 
+          data={data} 
+          pageSize={5} 
+        />
     </div>
   );
 }
